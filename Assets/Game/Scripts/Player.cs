df23e87c4cf710d258b752fefa9f68f9554c2dc8
Jeff Dalley <jeffdalley@gmail.com>
2019-04-07 00:37:17 -0500

    Player movement

--- Assets/Game/Scripts/Player.cs
+++ Assets/Game/Scripts/Player.cs
@@ -4,17 +4,48 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed;
+    [SerializeField]
+    private float speed = 5.0f;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        transform.position = new Vector3(0, 0, 0);
+        
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        transform.Translate(Vector3.right * (Time.deltaTime * speed));
+        Movement();
     }
+
+    private void Movement()
+    {
+        // Handle movement input
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
+        transform.Translate(Vector3.up * speed * verticalInput * Time.deltaTime);
+
+        // Limit player from going above half screen, or below bottom of the screen.
+        if (transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y <= -4.25)
+        {
+            transform.position = new Vector3(transform.position.x, -4.25f, 0);
+        }
+
+        // Wrap player left and right as they go off the screen
+        if (transform.position.x >= 10.7)
+        {
+            transform.position = new Vector3(-10.7f, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -10.7)
+        {
+            transform.position = new Vector3(10.7f, transform.position.y, 0);
+        }
+    }
+
 }
