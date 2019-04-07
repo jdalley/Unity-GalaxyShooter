7491aa728ab029c50c49c5ce0eca974f93f8b703
Jeff Dalley <jeffdalley@gmail.com>
2019-04-07 01:58:05 -0500

    Shootin' lasers

--- Assets/Game/Scripts/Player.cs
+++ Assets/Game/Scripts/Player.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private float speed = 5.0f;
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private float _speed = 5.0f;
+    [SerializeField]
+    private float _fireRate = 0.25f;
+    private float _nextFire = 0.0f;
 
     // Start is called before the first frame update
     private void Start()
@@ -17,6 +22,11 @@ public class Player : MonoBehaviour
     private void Update()
     {
         Movement();
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButton(0))
+        {
+            Shoot();
+        }
     }
 
     private void Movement()
@@ -24,8 +34,8 @@ public class Player : MonoBehaviour
         // Handle movement input
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
-        transform.Translate(Vector3.up * speed * verticalInput * Time.deltaTime);
+        transform.Translate(Vector3.right * _speed * horizontalInput * Time.deltaTime);
+        transform.Translate(Vector3.up * _speed * verticalInput * Time.deltaTime);
 
         // Limit player from going above half screen, or below bottom of the screen.
         if (transform.position.y > 0)
@@ -45,6 +55,15 @@ public class Player : MonoBehaviour
         else if (transform.position.x <= -10.7)
         {
             transform.position = new Vector3(10.7f, transform.position.y, 0);
+        }
+    }
+
+    private void Shoot()
+    {
+        if (Time.time > _nextFire)
+        {
+            Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.88f, 0), Quaternion.identity);
+            _nextFire = Time.time + _fireRate;
         }
     }
 
